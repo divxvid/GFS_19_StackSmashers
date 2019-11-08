9e866669fc0d02bb6b75a96ec3bda3a96724f0f2
2019201090-SoM <souptik.mondal@students.iiit.ac.in>
2019-11-08 15:41:59 +0530

    Modified version

--- reference_codes/chunks.py
+++ reference_codes/chunks.py
@@ -7,7 +7,7 @@ chunks_name = "image_chunks_"
 with open("image1.jpg", "rb") as fp1 :
     read_data = fp1.read(chunk_size)
     while read_data :
-        with open(f"{chunks_name}{str(no_of_files)}.jpg", "wb") as fp2 :
+        with open(f"{chunks_name}{no_of_files}.jpg", "wb") as fp2 :
             fp2.write(read_data)
         read_data = fp1.read(chunk_size)
         no_of_files = no_of_files + 1
